b601356b0ca3bf67d364c332333acb155e508625
minusie357 <minusie357@gmail.com>
2023-12-20 14:02:11 +0900

    [Project Setting] DirectX 12 App 기초 구동을 위한 "InitDirect3D" 관련 기능 추가

--- D3D12/D3D12/Main.cpp
+++ D3D12/D3D12/Main.cpp
@@ -1,25 +1,128 @@
+//***************************************************************************************
+// Init Direct3D.cpp by Frank Luna (C) 2015 All Rights Reserved.
+//
+// Demonstrates the sample framework by initializing Direct3D, clearing 
+// the screen, and displaying frame stats.
+//
+//***************************************************************************************
+
+#include <DirectXColors.h>
 #include "d3dApp.h"
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-    PSTR cmdLine, int showCmd)
+using namespace DirectX;
+
+class Direct3DApp : public D3DApp
 {
-    // Enable run-time memory check for debug builds.
+public:
+	Direct3DApp(HINSTANCE hInstance);
+	~Direct3DApp();
+
+	virtual bool Initialize()override;
+
+private:
+	virtual void OnResize()override;
+	virtual void Update(const GameTimer& gt)override;
+	virtual void Draw(const GameTimer& gt)override;
+
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-    try
-    {
-        //InitDirect3DApp theApp(hInstance);
-        //if (!theApp.Initialize())
-        //    return 0;
+	try
+	{
+		Direct3DApp theApp(hInstance);
+		if (!theApp.Initialize())
+			return 0;
 
-        //return theApp.Run();
-        return 0;
-    }
-    catch (DxException& e)
-    {
-        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
-        return 0;
-    }
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+Direct3DApp::Direct3DApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+{
+}
+
+Direct3DApp::~Direct3DApp()
+{
+}
+
+bool Direct3DApp::Initialize()
+{
+	if (!D3DApp::Initialize())
+		return false;
+
+	return true;
+}
+
+void Direct3DApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void Direct3DApp::Update(const GameTimer& gt)
+{
+
+}
+
+void Direct3DApp::Draw(const GameTimer& gt)
+{
+	// Reuse the memory associated with command recording.
+	// We can only reset when the associated command lists have finished execution on the GPU.
+	ThrowIfFailed(mDirectCmdListAlloc->Reset());
+
+	// A command list can be reset after it has been added to the command queue via ExecuteCommandList.
+	// Reusing the command list reuses memory.
+	ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+	// Indicate a state transition on the resource usage.
+	auto currentBackBuffer = CurrentBackBuffer();
+	auto presentToRenderTarget = CD3DX12_RESOURCE_BARRIER::Transition(currentBackBuffer,
+		D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	mCommandList->ResourceBarrier(1, &presentToRenderTarget);
+
+	// Set the viewport and scissor rect.  This needs to be reset whenever the command list is reset.
+	mCommandList->RSSetViewports(1, &mScreenViewport);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
+
+	// Clear the back buffer and depth buffer.
+	auto backBuffer = CurrentBackBufferView();
+	auto dsvBuffer = DepthStencilView();
+	mCommandList->ClearRenderTargetView(backBuffer, Colors::LightSteelBlue, 0, nullptr);
+	mCommandList->ClearDepthStencilView(dsvBuffer, D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+	// Specify the buffers we are going to render to.
+	mCommandList->OMSetRenderTargets(1, &backBuffer, true, &dsvBuffer);
+
+	// Indicate a state transition on the resource usage.
+	auto renderTargetToPresent = CD3DX12_RESOURCE_BARRIER::Transition(currentBackBuffer,
+		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+	mCommandList->ResourceBarrier(1, &renderTargetToPresent);
+
+	// Done recording commands.
+	ThrowIfFailed(mCommandList->Close());
+
+	// Add the command list to the queue for execution.
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	// swap the back and front buffers
+	ThrowIfFailed(mSwapChain->Present(0, 0));
+	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+
+	// Wait until frame commands are complete.  This waiting is inefficient and is
+	// done for simplicity.  Later we will show how to organize our rendering code
+	// so we do not have to wait per frame.
+	FlushCommandQueue();
 }
